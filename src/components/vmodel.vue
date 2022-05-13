1b3d781ef1f22b64d06d72fb110bf91036cbcd8a
SongkietSRS <91242995+SongkietSRS@users.noreply.github.com>
2022-05-13 15:43:37 +0700

    Initial commit

---
+++ src/components/vmodel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>V-Model</h1>
+    <input v-model="massage" />
+    <h3>{{ massage }}</h3>
+  </div>
+</template>>
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      massage: "",
+    };
+  },
+};
+</script>>
---
+++ src/components/von.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>V-On</h1>
+    <h2>{{ massage }}</h2>
+    <button v-on:click="add()">Add</button>
+  </div>
+</template>>
+
+
+<script>
+export default {
+  data() {
+    return {
+      massage: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.massage = this.massage + 1;
+    },
+  },
+};
+</script>>
